92711c9ead99568b302d2becc26d6a0f5392e838
skiera <skiera@883fd64d-5c66-4666-9b8f-b541fcdf2f7e>
2010-04-11 14:27:23 +0000

    Closed 26: Switch to Eclipse Public License (EPL)

--- src/org/phpsrc/eclipse/pti/tools/phpunit/ui/actions/RunTestCaseAction.java
+++ src/org/phpsrc/eclipse/pti/tools/phpunit/ui/actions/RunTestCaseAction.java
@@ -1,31 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2010, Sven Kiera
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * - Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimer.
- * - Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- * - Neither the name of the Organisation nor the names of its contributors may
- *   be used to endorse or promote products derived from this software without
- *   specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (c) 2009, 2010 Sven Kiera
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
-package org.phpsrc.eclipse.pti.tools.phpunit.ui.actions;
 
+package org.phpsrc.eclipse.pti.tools.phpunit.ui.actions;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IResource;
@@ -39,6 +20,7 @@ import org.phpsrc.eclipse.pti.tools.phpunit.core.PHPUnit;
 import org.phpsrc.eclipse.pti.tools.phpunit.validator.PHPUnitValidator;
 import org.phpsrc.eclipse.pti.ui.Logger;
 import org.phpsrc.eclipse.pti.ui.actions.ResourceAction;
+
 
 public class RunTestCaseAction extends ResourceAction {
 
--- src/org/phpsrc/eclipse/pti/tools/phpunit/ui/preferences/PHPUnitPreferencePage.java
+++ src/org/phpsrc/eclipse/pti/tools/phpunit/ui/preferences/PHPUnitPreferencePage.java
@@ -1,31 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2010, Sven Kiera
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * - Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimer.
- * - Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- * - Neither the name of the Organisation nor the names of its contributors may
- *   be used to endorse or promote products derived from this software without
- *   specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (c) 2009, 2010 Sven Kiera
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
-package org.phpsrc.eclipse.pti.tools.phpunit.ui.preferences;
 
+package org.phpsrc.eclipse.pti.tools.phpunit.ui.preferences;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.php.internal.ui.preferences.PropertyAndPreferencePage;
@@ -35,6 +16,7 @@ import org.eclipse.ui.preferences.IWorkbenchPreferenceContainer;
 import org.phpsrc.eclipse.pti.core.launching.PHPToolLauncher;
 import org.phpsrc.eclipse.pti.tools.phpunit.PHPUnitPlugin;
 import org.phpsrc.eclipse.pti.tools.phpunit.core.PHPUnit;
+
 
 public class PHPUnitPreferencePage extends PropertyAndPreferencePage {
 
@@ -52,7 +34,7 @@ public class PHPUnitPreferencePage extends PropertyAndPreferencePage {
 	 * org.eclipse.jface.dialogs.IDialogPage#createControl(org.eclipse.swt.widgets
 	 * .Composite)
 	 */
-	
+
 	public void createControl(Composite parent) {
 		IWorkbenchPreferenceContainer container = (IWorkbenchPreferenceContainer) getContainer();
 		fConfigurationBlock = new PHPUnitConfigurationBlock(getNewStatusChangedListener(), getProject(), container);
@@ -66,7 +48,7 @@ public class PHPUnitPreferencePage extends PropertyAndPreferencePage {
 	 * @seeorg.eclipse.jdt.internal.ui.preferences.PropertyAndPreferencePage#
 	 * createPreferenceContent(org.eclipse.swt.widgets.Composite)
 	 */
-	
+
 	protected Control createPreferenceContent(Composite composite) {
 		return fConfigurationBlock.createContents(composite);
 	}
@@ -77,7 +59,7 @@ public class PHPUnitPreferencePage extends PropertyAndPreferencePage {
 	 * @seeorg.eclipse.jdt.internal.ui.preferences.PropertyAndPreferencePage#
 	 * hasProjectSpecificOptions(org.eclipse.core.resources.IProject)
 	 */
-	
+
 	protected boolean hasProjectSpecificOptions(IProject project) {
 		return fConfigurationBlock.hasProjectSpecificOptions(project);
 	}
@@ -88,7 +70,7 @@ public class PHPUnitPreferencePage extends PropertyAndPreferencePage {
 	 * @seeorg.eclipse.jdt.internal.ui.preferences.PropertyAndPreferencePage#
 	 * getPreferencePageID()
 	 */
-	
+
 	protected String getPreferencePageID() {
 		return PREF_ID;
 	}
@@ -99,7 +81,7 @@ public class PHPUnitPreferencePage extends PropertyAndPreferencePage {
 	 * @seeorg.eclipse.jdt.internal.ui.preferences.PropertyAndPreferencePage#
 	 * getPropertyPageID()
 	 */
-	
+
 	protected String getPropertyPageID() {
 		return PROP_ID;
 	}
@@ -110,7 +92,7 @@ public class PHPUnitPreferencePage extends PropertyAndPreferencePage {
 	 * @seeorg.eclipse.jdt.internal.ui.preferences.PropertyAndPreferencePage#
 	 * enableProjectSpecificSettings(boolean)
 	 */
-	
+
 	protected void enableProjectSpecificSettings(boolean useProjectSpecificSettings) {
 		// Order is important!
 		super.enableProjectSpecificSettings(useProjectSpecificSettings);
@@ -122,7 +104,7 @@ public class PHPUnitPreferencePage extends PropertyAndPreferencePage {
 	/*
 	 * @see org.eclipse.jface.preference.IPreferencePage#performDefaults()
 	 */
-	
+
 	protected void performDefaults() {
 		super.performDefaults();
 		if (fConfigurationBlock != null) {
@@ -133,7 +115,7 @@ public class PHPUnitPreferencePage extends PropertyAndPreferencePage {
 	/*
 	 * @see org.eclipse.jface.preference.IPreferencePage#performOk()
 	 */
-	
+
 	public boolean performOk() {
 		if (fConfigurationBlock != null && !fConfigurationBlock.performOk()) {
 			return false;
@@ -147,7 +129,7 @@ public class PHPUnitPreferencePage extends PropertyAndPreferencePage {
 	/*
 	 * @see org.eclipse.jface.preference.IPreferencePage#performApply()
 	 */
-	
+
 	public void performApply() {
 		if (fConfigurationBlock != null) {
 			fConfigurationBlock.performApply();
@@ -159,7 +141,7 @@ public class PHPUnitPreferencePage extends PropertyAndPreferencePage {
 	 * 
 	 * @see org.eclipse.jface.dialogs.DialogPage#dispose()
 	 */
-	
+
 	public void dispose() {
 		if (fConfigurationBlock != null) {
 			fConfigurationBlock.dispose();
@@ -174,7 +156,7 @@ public class PHPUnitPreferencePage extends PropertyAndPreferencePage {
 	 * org.eclipse.jdt.internal.ui.preferences.PropertyAndPreferencePage#setElement
 	 * (org.eclipse.core.runtime.IAdaptable)
 	 */
-	
+
 	public void setElement(IAdaptable element) {
 		super.setElement(element);
 		setDescription(null); // no description for property page
